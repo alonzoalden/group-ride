348e7799b3ccb9f7a5b6f2dd07baaee0620c81da
Alonzo Alden <alonzoalden@gmail.com>
2019-05-01 14:44:50 -0700

    s

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 
 import { MatButtonModule, MatDatepickerModule } from '@angular/material';
